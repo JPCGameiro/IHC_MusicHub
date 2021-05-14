cf3e92172d324909bfbdb25ff62887d433bb6f8c
Pedro Abreu <pedro.abreu@ua.pt>
2021-05-14 16:21:28 +0100

    add packages, u may need to npm install, try to do a post

--- musicHub/src/app/app.module.ts
+++ musicHub/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatCardModule } from '@angular/material/card';
+
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
