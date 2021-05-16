4b7c65a39fd76a25818fd4a4ae9afba6a1ec7f74
Pedro Abreu <pedro.abreu@ua.pt>
2021-05-16 14:47:49 +0100

    general structure

--- musicHub/src/app/app.module.ts
+++ musicHub/src/app/app.module.ts
@@ -9,13 +9,15 @@ import { CardComponent } from './card/card.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { MessengerComponent } from './messenger/messenger.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
     NavbarComponent,
-    JumbotronComponent
+    JumbotronComponent,
+    MessengerComponent
   ],
   imports: [
     BrowserModule,
