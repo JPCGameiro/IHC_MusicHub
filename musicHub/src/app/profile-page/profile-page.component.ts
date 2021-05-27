678e5ae88fdb98c55314b18f46d30e3e65d8bcd4
Juan Lessa <juanvlessa@ua.pt>
2021-05-27 13:16:46 +0100

    html and css new post modal incomplete

--- musicHub/src/app/profile-page/profile-page.component.ts
+++ musicHub/src/app/profile-page/profile-page.component.ts
@@ -3,13 +3,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
-  styleUrls: ['./profile-page.component.css']
+  styleUrls: ['./profile-page.component.css'],
 })
 export class ProfilePageComponent implements OnInit {
+  // js modal code, convert to TS
+  // Get the modal
+  // const modal = document.getElementById("myModal");
+  //
+  // Get the button that opens the modal
+  // const btn = document.getElementById("myBtn");
+  //
+  // Get the <span> element that closes the modal
+  // const span = document.getElementsByClassName("close")[0];
+  //
+  // When the user clicks on the button, open the modal
+  // btn.onclick = function() {
+  //   modal.style.display = "block";
+  // }
+  //
+  // When the user clicks on <span> (x), close the modal
+  // span.onclick = function() {
+  //   modal.style.display = "none";
+  // }
+  //
+  // When the user clicks anywhere outside of the modal, close it
+  // window.onclick = function(event) {
+  //   if (event.target == modal) {
+  //     modal.style.display = "none";
+  //   }
+  // }
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
