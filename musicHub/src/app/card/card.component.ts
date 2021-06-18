792d0a0e18ea267663c0449a3aff8cf9241bc4ba
Pedro Abreu <pedro.abreu@ua.pt>
2021-06-18 17:15:49 +0100

    changed potos

--- musicHub/src/app/card/card.component.ts
+++ musicHub/src/app/card/card.component.ts
@@ -12,8 +12,20 @@ export class CardComponent implements OnInit {
 
   examplePost : Post = {
     username: "Pedro Abreu",
-    smldescription: "pequena descrição",
-    description: "descrição completa do post, não sei organizar o backend, gerar uma data de posts??, para fazer: for chamar posts e fazer passar informação de um componente pai para filho"
+    smldescription: "Work of art!",
+    description: "Vejam só a minha capa de albúm"
+  }
+
+  examplePost1 : Post = {
+    username: "Green Day",
+    smldescription: "Check our new single!",
+    description: "The single Pollyanna is already available on all platforms. #stay green"
+  }
+  
+  examplePost2 : Post = {
+    username: "Ana Simões",
+    smldescription: "What a blast!!",
+    description: "Last night U2 concert was out of this world! propz to them!"
   }
 
   constructor() { }
