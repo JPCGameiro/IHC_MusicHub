448870622a66acb70322e32a0e3f0824c6b43aad
Pedro Abreu <pedro.abreu@ua.pt>
2021-06-19 14:02:27 +0100

    add mensseger page

--- musicHub/src/app/mensseger-page/mensseger-page.component.ts
+++ musicHub/src/app/mensseger-page/mensseger-page.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-mensseger-page',
   templateUrl: './mensseger-page.component.html',
-  styleUrls: ['./mensseger-page.component.css']
+  styleUrls: ['./mensseger-page.component.scss',
+              '../../../src/styles/styles.scss']
 })
 export class MenssegerPageComponent implements OnInit {
 
